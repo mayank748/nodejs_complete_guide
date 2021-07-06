71fb66ca18ad93db423764c0471754e063e5ca10
Mayank Singh <smayank748@gmail.com>
2021-07-06 06:53:23 +0530

    rest and spread operator

--- first_app.js
+++ first_app.js
@@ -1,2 +1,14 @@
 const fs=require('fs');
 fs.writeFileSync('hello,txt','Hello from Node.js')
+
+// spread operator
+const hobbies=['Sports','Cooking'];
+const copiedArray=[...hobbies];
+console.log(copiedArray);
+
+// rest operator
+const toArray=(...args)=>{
+    return args;
+};
+
+console.log(toArray(1,2,3,4));
