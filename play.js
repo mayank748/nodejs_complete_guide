79c3a622745750f74c305e509f7c828d3dd71018
Mayank Singh <smayank748@gmail.com>
2021-07-06 07:11:54 +0530

    destructing

--- play.js
+++ play.js
@@ -5,4 +5,19 @@ const person={
         console.log('Hi, I am '+this.name);
     }
 };
-person.greet();
+// person.greet();
+
+// destructing
+// object 
+const printName=({name})=>{
+    console.log(name);
+    // console.log(greet());
+}
+printName(person);
+
+const {name,age}=person;
+console.log(name,age);
+
+const hobbies=['Sports','Cooking'];
+const [hobby1,hobby2]=hobbies;
+console.log(hobby1,hobby2);
