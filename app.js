212edcec0a7f5d0d513f4eefd13921c732f96c2a
Mayank <smayank748@gmail.com>
2021-09-21 23:07:15 +0530

    adding different middle ware

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express=require('express');
 const app=express();
 
 //Place for middleware
-app.use((req,res,next)=>{
-    console.log('In the middleware first')
-    next();//Allows the middleware to continue to the next middleware in line
+
+app.use('/add-product',(req,res,next)=>{
+    console.log('In the middleware second')
+    res.send('<h1>Hello from add product</h1>')
 });
 
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     console.log('In the middleware second')
     res.send('<h1>Hello from express js</h1>')
 });
