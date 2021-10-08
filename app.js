4bf2fb996383a2bc438701d269631570dc1566d6
Mayank Singh <smayank748@gmail.com>
2021-10-08 23:51:23 +0530

    implementation of routes

--- app.js
+++ app.js
@@ -3,24 +3,19 @@ const bodyParser=require('body-parser');
 
 const app=express();
 
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 //Place for middleware
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    console.log('In the middleware second')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>')
-});
+// app.use('/',(req,res,next)=>{
+//     console.log('In the middleware second')
+//     res.send('<h1>Hello from express js</h1>')
+// });
 
-app.post('/product',(req,res,next)=>{
-    console.log('In the product middleware');
-    console.log(req.body);
-    res.redirect('/')
-});
-
-app.use('/',(req,res,next)=>{
-    console.log('In the middleware second')
-    res.send('<h1>Hello from express js</h1>')
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 // const server=http.createServer(app);
 // server.listen(3000);
