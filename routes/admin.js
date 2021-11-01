398c340dbf85bf5486492d7d15ca4114f0ac7492
Mayank Singh <smayank748@gmail.com>
2021-11-01 23:16:16 +0530

    using helper function to navigate

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path=require('path');
+const rootDir=require('../util/Path');
 const router = express.Router();
 
 router.get('/add-product',(req,res,next)=>{
     //console.log('In the middleware second')
     //res.send('<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>')
-    res.sendFile(path.join(__dirname,'..','views','add-product.html'))
+    // res.sendFile(path.join(__dirname,'..','views','add-product.html'))
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
 });
 
 router.post('/product',(req,res,next)=>{
-    console.log('In the product middleware');
+    //console.log('In the product middleware');
     console.log(req.body);
     res.redirect('/')
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path=require('path');
+const rootDir=require('../util/Path');
 
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
     // res.send('<h1>Hello from Express!</h1>')
-    res.sendFile(path.join(__dirname,'..','views','shop.html'))
+    // res.sendFile(path.join(__dirname,'..','views','shop.html'))
+    res.sendFile(path.join(rootDir,'views','shop.html'))
 })
 
 module.exports = router; 
