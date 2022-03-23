9bc347092eb9700486caaeb8cc80c7547bfd95bd
Mayank <smayank748@gmail.com>
2022-03-23 17:53:30 +0530

    adding pug to display the data

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,8 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
     console.log('In the middleware second')
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', { pageTitle: 'add-product', path: '/admin/add-product' });
 });
 
 router.post('/add-product', (req, res, next) => {
